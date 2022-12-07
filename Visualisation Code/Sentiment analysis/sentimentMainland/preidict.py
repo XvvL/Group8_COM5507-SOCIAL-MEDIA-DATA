59bd446ab69b434414f120687985f5b98ffcfce2
XvvL <119738028+XvvL@users.noreply.github.com>
2022-12-07 14:08:18 +0800

    Add files via upload

---
+++ Visualisation Code/Sentiment analysis/sentimentMainland/preidict.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 10:28:41 2020
+
+@author: cm
+"""
+
+from sentimentMainland.networks import SentimentAnalysis
+
+
+SA = SentimentAnalysis()
+
+
+def predict(sent):
+    """
+    1: positif
+    0: neutral
+    -1: negatif
+    """
+    score1,score0 = SA.normalization_score(sent)
+    if score1 == score0:
+        result = 0
+    elif score1 > score0:
+        result = 1
+    elif score1 < score0:
+        result = -1
+    return result
+        
+
+if __name__ =='__main__':
+    f = open(r"weibo-comment.txt", "r", encoding="UTF-8")
+    data = f.read()
+    print(predict(data))
+
+
+
+
